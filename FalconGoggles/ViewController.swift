ab1f1f8518257098ae2c6b6d5ea3bf39dc445a68
Isaiah Butcher <isaiah.jr.ib@gmail.com>
2017-11-12 15:43:10 -0700

    Initial Commit

---
+++ FalconGoggles/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FalconGoggles
+//
+//  Created by Isaiah Butcher on 11/12/17.
+//  Copyright © 2017 Isaiah Butcher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
