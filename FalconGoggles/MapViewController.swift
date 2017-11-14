5f51b23b2fb665816f0aeca1c71d85f9a315b5b8
M2A User <m2auser@Mobiles-iMac-24.local>
2017-11-13 19:20:55 -0700

    Testing map

---
+++ FalconGoggles/MapViewController.swift
@@ -0,0 +1,46 @@
+//
+//  MapViewController.swift
+//  FalconGoggles
+//
+//  Created by M2A User on 11/13/17.
+//  Copyright © 2017 Isaiah Butcher. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let initialLocation = CLLocation(latitude:39.008493, longitude: -104.888742)
+    
+    let regionRadius: CLLocationDistance = 1000
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func centerMapOnLocation(location: CLLocation){
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
