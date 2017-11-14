81eff7ae5621644961a7c6b0f9a55071613ec985
M2A User <m2auser@Mobiles-iMac-24.local>
2017-11-13 20:22:27 -0700

    ...

--- FalconGoggles/MapViewController.swift
+++ FalconGoggles/MapViewController.swift
@@ -51,24 +51,26 @@ class MapViewController: UIViewController {
 }
 
 extension MapViewController: MKMapViewDelegate{
-    func mapView(_mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        
-        guard let annotation = annotation as? Monument else{return nil}
-        
-        let identifier = "marker"
-        var view: MKMarkerAnnotationView
-        
-        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView{
-            dequeuedView.annotation = annotation
-            view = dequeuedView
-        } else{
-            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
-            view.canShowCallout = true
-            view.calloutOffset = CGPoint(x: -5, y: 5)
-            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            // 2
+            guard let annotation = annotation as? Monument else { return nil }
+            // 3
+            let identifier = "marker"
+            var view: MKMarkerAnnotationView
+            // 4
+            if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+                as? MKMarkerAnnotationView { 
+                dequeuedView.annotation = annotation
+                view = dequeuedView
+            } else {
+                // 5
+                view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                view.canShowCallout = true
+                view.calloutOffset = CGPoint(x: -5, y: 5)
+                view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            }
+            return view
         }
-        return view
-    }
     
 //    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
 //        let location = view.annotation as! Monument
